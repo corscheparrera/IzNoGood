823905592ded438b815176948a9441f712ea76d7
Maxime Lussier <m.lussier@live.ca>
2017-11-14 19:15:14 -0500

    Fixed img rendering

--- src/Components/JSX/TestSucceeded.jsx
+++ src/Components/JSX/TestSucceeded.jsx
@@ -7,7 +7,9 @@ class TestSucceeded extends Component {
         <h2>
           Your product seems clean. Your list of ingredients passed our tests
         </h2>
-        <img src="flag.jpeg" alt="" height="42" width="42" />
+
+        <img src={require("./flag.jpeg")} />
+
         <button>Want to save your product ? </button>
       </div>
     );
